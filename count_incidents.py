c72bc917e46b959a7d31fa61734e621823f353fa
tmp-alan-github <siliconmicrodrop@gmail.com>
2018-07-16 16:02:20 -0400

    Post call on Monday - pushing changes since submission.

--- count_incidents.py
+++ count_incidents.py
@@ -2,36 +2,63 @@ from pprint import pprint
 import pandas as pd
 
 
+def get_population_from_zip( zip_code ):
+	with open("data/zips_pop_nyc.csv", "r") as f:
+		for line in f:
+			split = line.split(":")
+			if (split[0]) == zip_code:
+				# print(split[0],split[1])
+				return split[1]
+
+
 def count_borough_incidents( in_f='data/311.csv', size=1 ):
-    population_dict = {"bronx": 1471160, "brooklyn": 2648771, "manhattan": 1664727, 'queens': 23585832,
-                       'staten island': 479558}
+	# Below populations grabbed from Census.gov, https://bit.ly/2mrBYQl
+	population_dict = {"bronx": 1471160, "brooklyn": 2648771, "manhattan": 1664727, 'queens': 23585832,
+	                   'staten island': 479558}
 
-    counting_dict = {"bronx": 0, "brooklyn": 0, "manhattan": 0, 'queens': 0, 'staten island': 0}
-    reader = pd.read_table(in_f, sep=',', skip_blank_lines=True, memory_map=True, chunksize=size)
+	counting_dict = {"bronx": 0, "brooklyn": 0, "manhattan": 0, 'queens': 0, 'staten island': 0}
+	reader = pd.read_table(in_f, sep=',', skip_blank_lines=True, memory_map=True, chunksize=size, nrows=10000)
 
-    for chunk in reader:
-        borough = str(chunk['Borough'].values[0]).strip().lower()
-        try:
-            counting_dict[borough] += 1
-        except KeyError:
-            print("unspecified: ", borough)
-        pprint(dict(counting_dict))
-        print("------")
+	for chunk in reader:
+		borough = str(chunk['Borough'].values[0]).strip().lower()
+		try:
+			counting_dict[borough] += 1
+		except KeyError:
+			print("unspecified: ", borough)
+		pprint(dict(counting_dict))
+		print("------")
 
-    counting_dict_testing = {'bronx': 31576, 'brooklyn': 52929, 'manhattan': 34820, 'queens': 40339,
-                             'staten island': 9088}
-    print("Complaints per 10,000 people")
-    for key in population_dict.keys():
-        result = int(counting_dict[key] * (100000 / (population_dict[key])))
-        print(key, " - ", result)
+	counting_dict_testing = {'bronx': 31576, 'brooklyn': 52929, 'manhattan': 34820, 'queens': 40339,
+	                         'staten island': 9088}
+
+	print("--- Complaints per 10,000 people in Boroughs --- \n")
+	for key in population_dict.keys():
+
+		result = float("{0:.2f}".format(counting_dict[key] / (population_dict[key]) * 100000))
+		print(key, " - ", result)
 
 
 def count_zip_incidents( in_f='data/311.csv', size=1 ):
-    reader = pd.read_table(in_f, sep=',', skip_blank_lines=True, chunksize=size)
-    zip_counter = {}
-    for chunk in reader:
-        zip = str(chunk['Incident Zip'].values[0]).split(",")[0].strip()
-        if ("nan" != zip):
-            zip_counter.setdefault(zip, 1)
-            zip_counter[zip] += 1
-            pprint(dict(zip_counter))
+	reader = pd.read_table(in_f, sep=',', skip_blank_lines=True, chunksize=size)
+	zip_counter = {}
+	zips = 0
+	for chunk in reader:
+		try:
+			zips = (str(float(chunk['Incident Zip'].values[0])).split(",")[0].strip())[:-2]
+			print(zips)
+		except Exception as e:
+			print("ERROR (Likely NAN),:", e, zips)
+
+		if zips.isnumeric():
+			zip_counter.setdefault(zips, 1)
+			zip_counter[zips] += 1
+			pprint(dict(zip_counter))
+
+	for zip_tmp in zip_counter:
+		try:
+			zip_counter[zip_tmp] = (float(zip_counter[zip_tmp]) / float(get_population_from_zip(zip_tmp))) * 10000.0
+		except TypeError:
+			print("Type Error:", zip_tmp)
+	print("\n ----Sorted by most incidents per zip (top 10)  ---- \n")
+	for w in sorted(zip_counter, key=zip_counter.get, reverse=True)[0:10]:
+		print(w, " - ", float("{0:.2f}".format(zip_counter[w])))
--- main.py
+++ main.py
@@ -8,79 +8,84 @@ from count_incidents import count_borough_incidents, count_zip_incidents
 
 
 def write_set( zipset ):
-    with open('nyc_zips.txt', 'w') as f:
-        for i in zipset:
-            f.write(str(i) + "\n")
+	with open('nyc_zips.txt', 'w') as f:
+		for i in zipset:
+			f.write(str(i) + "\n")
+
+
+def write_dict_to_csv( tmp_dict, out_f="output.csv" ):
+	with open(out_f, 'w') as f:  # Just use 'w' mode in 3.x
+		w = csv.DictWriter(f, tmp_dict.keys())
+		w.writeheader()
+		w.writerow(tmp_dict)
 
 
 # MAIN Parsing logic - ending results in CSV created to map Zip Codes, Incident Occurences
-def preprocessing_csv( in_f, size, is_zip_root, out_f='output.csv' ):
-    start_timer = time()
-    zip_set = set()
+def preprocessing_csv( in_f, size, is_zip_root ):
+	start_timer = time()
+	zip_set = set()
 
-    master_dict = defaultdict(lambda: defaultdict(lambda: Counter()))
+	# Default dict with a collection counter to assist
+	master_dict = defaultdict(lambda: defaultdict(lambda: Counter()))
 
-    reader = pd.read_csv(in_f, sep=',', skip_blank_lines=True, memory_map=True, chunksize=size)
-    for chunk in reader:
+	# Much room for improvement among these parameters
+	reader = pd.read_csv(in_f, sep=',', skip_blank_lines=True, memory_map=True, chunksize=size)
+	for chunk in reader:
+		complaint = str(chunk['Complaint Type'].values[0]).split(",")[0].strip().replace("DOF", '')
+		if (is_zip_root):
+			zip = str(chunk['Incident Zip'].values[0]).split(",")[0].strip()
+			root = zip
+		else:
+			borough = str(chunk['Borough'].values[0]).strip()
+			root = borough
 
-        complaint = str(chunk['Complaint Type'].values[0]).split(",")[0].strip().replace("DOF", '')
-        if (is_zip_root):
-            zip = str(chunk['Incident Zip'].values[0]).split(",")[0].strip()
-            root = zip
-        else:
-            borough = str(chunk['Borough'].values[0]).strip()
-            root = borough
+		if ("nan" not in root.lower() and "unspecified" not in root.lower()):
+			try:
+				if root in zip_set:
+					master_dict[root][complaint] += 1
+				else:
+					master_dict[root] = {complaint: 1}
+					zip_set.add((root))
+			except KeyError:
+				# print("KEY ERROR", master_dict[root])
+				# print(root, " ", complaint)
+				master_dict[root][complaint] = 1  # print(root, " ", complaint)
 
-        if ("nan" not in root.lower() and "unspecified" not in root.lower()):
-            try:
-                if root in zip_set:
-                    master_dict[root][complaint] += 1
-                else:
-                    master_dict[root] = {complaint: 1}
-                    zip_set.add((root))
-            except KeyError:
-                # print("KEY ERROR", master_dict[root])
-                # print(root, " ", complaint)
-                master_dict[root][complaint] = 1  # print(root, " ", complaint)
+		# Refresh Rate of Prints (.5 seconds)
+		if time() - start_timer > .5:
+			pprint(dict(master_dict))
+			start_timer = time()
 
-        # Refresh Rate of Prints (.5 seconds)
-        if time() - start_timer > .5:
-            pprint(dict(master_dict))
-            start_timer = time()
-
-    # Write dictionary accumulated into csv upon completion of parsing
-    if (is_zip_root):
-        write_set(zip_set)
-    with open(out_f, 'w') as f:  # Just use 'w' mode in 3.x
-        w = csv.DictWriter(f, master_dict.keys())
-        w.writeheader()
-        w.writerow(master_dict)
-
+	# Write dictionary accumulated into csv upon completion of parsing
+	if (is_zip_root):
+		write_set(zip_set)
+		write_dict_to_csv(master_dict)
 
 # Start the function call
 def main():
-    zip_root = None
-    print("\n---Written by Alan Steinberg---\n")
-    while (True):
-        print("1. Aggregate incidents per zip code")
-        print("2. Aggregate incidents per boroughs")
-        print("3. Analyze incidents per 10k capita by zip")
-        print("4. Analyze incidents per 10k capita or by Borough")
-        kb_user = input("   Enter Choice:").lower().strip()
+	zip_root = None
+	print("\n---Written by Alan Steinberg---\n")
+	while (True):
+		print("\n1. Aggregate incidents per zip code")
+		print("2. Aggregate incidents per boroughs")
+		print("3. Analyze incidents per 10k capita by zip")
+		print("4. Analyze incidents per 10k capita  by Borough")
 
-        if (kb_user == "1"):
-            zip_root = True
-            break
-        elif (kb_user == "2"):
-            zip_root = False
-            break
-        elif (kb_user == "3"):
-            count_zip_incidents('data/311.csv', 100)
-        elif (kb_user == "4"):
-            count_borough_incidents('data/311.csv', 100)
-        else:
-            print("Bad input! Either Z or B are to be chosen.")
-    preprocessing_csv("data/311.csv", 1, zip_root)
+		kb_user = input("   Enter Choice:").lower().strip()
+
+		if (kb_user == "1"):
+			zip_root = True
+			break
+		elif (kb_user == "2"):
+			zip_root = False
+			break
+		elif (kb_user == "3"):
+			count_zip_incidents('data/311.csv', )
+		elif (kb_user == "4"):
+			count_borough_incidents('data/311.csv', )
+		else:
+			print("Bad input! Either (1,2,3,4) are to be chosen.")
+		preprocessing_csv("data/311.csv", 1, zip_root)
 
 
 main()
